50c713ccee42eae5ad39c4fe59345a810b3e73ea
adekurniawann <56309238+adekurniawann@users.noreply.github.com>
2022-11-04 16:25:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
     "html"
     "log"
     "net/http"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
+    fmt.Println("Simple App")
+    db, err := sql.Open("mysql", "<username>:<password>@tcp(database_server_url:port_number)")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+    
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
     })
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
