c02cdd852e745437c10d9f0c47347e65fed71442
adekurniawann <56309238+adekurniawann@users.noreply.github.com>
2022-11-04 17:09:34 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 )
 
 func main() {
-
-    fmt.Println("Simple App")
-    db, err := sql.Open("mysql", "<username>:<password>@tcp(database_server_url:port_number)")
-    if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
     
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
